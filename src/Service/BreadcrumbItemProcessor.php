04c1cf4db3ba00ad5a9add82adb786bdc1a1f85d
Paul Kepinski <paul.kepinski@orange.fr>
2022-03-13 16:31:20 +0100

    Allow parsing multiple parameters

--- src/Service/BreadcrumbItemProcessor.php
+++ src/Service/BreadcrumbItemProcessor.php
@@ -53,7 +53,7 @@ class BreadcrumbItemProcessor
     public function process(BreadcrumbItem $item, array $variables): ProcessedBreadcrumbItem
     {
         // Process the label
-        if ($item->getLabel() && $item->getLabel()[0] === '$') {
+        if ($item->getLabel() && $this->isValueParsable($item->getLabel())) {
             $processedLabel = $this->parseValue($item->getLabel(), $variables);
         } elseif (!$item->getLabel() || $item->getTranslationDomain() === false) {
             $processedLabel = $item->getLabel();
@@ -70,7 +70,7 @@ class BreadcrumbItemProcessor
             }
         }
         foreach ($item->getRouteParams() ?: [] as $key => $value) {
-            if ($value[0] === '$') {
+            if ($this->isValueParsable($value)) {
                 $params[$key] = $this->parseValue($value, $variables);
             } else {
                 $params[$key] = $value;
@@ -86,6 +86,23 @@ class BreadcrumbItemProcessor
         return new ProcessedBreadcrumbItem($processedLabel, $processedUrl);
     }
 
+    // Regex used to match parsable values (based on: https://www.php.net/manual/en/functions.user-defined.php)
+    private const PARSABLE_VALUE_REGEX = '/\$[a-zA-Z_\x80-\xff][a-zA-Z0-9_\x80-\xff]*(\.[a-zA-Z_\x80-\xff][a-zA-Z0-9_\x80-\xff]*)*/';
+
+    /**
+     * Check if the given variable is a parsable string
+     *
+     * @param mixed $expression
+     */
+    private function isValueParsable($expression): bool
+    {
+        if (!is_string($expression)) {
+            return false;
+        }
+
+        return preg_match_all(self::PARSABLE_VALUE_REGEX, $expression) > 0;
+    }
+
     /**
      * Returns the value contained in the variable name (with optional property path) of the given expression.
      *
@@ -93,21 +110,23 @@ class BreadcrumbItemProcessor
      */
     private function parseValue(string $expression, array $variables)
     {
-        $components = explode('.', $expression, 2);
-        $variableName = substr($components[0], 1); // Remove the $ prefix;
-        $propertyPath = $components[1] ?? null;
+        return preg_replace_callback(self::PARSABLE_VALUE_REGEX, function($matches) use ($variables) {
+            $components = explode('.', $matches[0], 2);
+            $variableName = substr($components[0], 1); // Remove the $ prefix;
+            $propertyPath = $components[1] ?? null;
 
-        if (!array_key_exists($variableName, $variables)) {
-            throw new \RuntimeException('The variables array passed to process the breadcrumb item does not have'
-                . ' variable "' . $variableName . '". Make sure you are passing that variable to the template in which'
-                . ' this breadcrumb is rendered.');
-        }
+            if (!array_key_exists($variableName, $variables)) {
+                throw new \RuntimeException('The variables array passed to process the breadcrumb item does not have'
+                    . ' variable "' . $variableName . '". Make sure you are passing that variable to the template in which'
+                    . ' this breadcrumb is rendered.');
+            }
 
-        if (!$propertyPath) {
-            // If this is a "top level" variable, return its value directly.
-            return $variables[$variableName];
-        }
+            if (!$propertyPath) {
+                // If this is a "top level" variable, return its value directly.
+                return $variables[$variableName];
+            }
 
-        return $this->propertyAccessor->getValue($variables[$variableName], $propertyPath);
+            return $this->propertyAccessor->getValue($variables[$variableName], $propertyPath);
+        }, $expression);
     }
 }
