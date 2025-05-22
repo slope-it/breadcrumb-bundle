c644a08df43fdaaaff0705f70ff1228cef0b3a94
Andrea Sprega <andrea.sprega@slope.it>
2025-05-22 10:38:00 +0200

    Fix deprecation message appeared in Symfony 7.2

--- src/DependencyInjection/SlopeItBreadcrumbExtension.php
+++ src/DependencyInjection/SlopeItBreadcrumbExtension.php
@@ -5,7 +5,7 @@ namespace SlopeIt\BreadcrumbBundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 
 class SlopeItBreadcrumbExtension extends Extension
 {
